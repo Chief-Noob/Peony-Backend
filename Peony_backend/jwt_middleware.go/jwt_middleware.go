266693c1e6bec9c8780cbd931063ad96da9ba977
Yan tin Chen <yantinchen@Yans-MacBook-Pro.local>
2020-11-25 11:57:35 +0800

    [feat/user] jwt_middleware exception handle

--- Peony_backend/jwt_middleware.go/jwt_middleware.go
+++ Peony_backend/jwt_middleware.go/jwt_middleware.go
@@ -3,8 +3,9 @@ package jwt_middleware
 import (
 	"Peony/Peony_backend/models/entity"
 	"Peony/config"
-	"fmt"
 	"strings"
+
+	"regexp"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -14,7 +15,7 @@ var jwtSecret = []byte(config.GetSecretKey())
 
 func JwtAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.Request.Header.Get("Authentication")
+		authHeader := c.Request.Header.Get("Authetication")
 		if len(authHeader) == 0 {
 			c.JSON(400, gin.H{
 				"error": "No authentication header.",
@@ -22,24 +23,34 @@ func JwtAuth() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+		mat, err := regexp.MatchString(`token.*`, authHeader)
 
+		if err != nil || mat == false {
+			c.JSON(401, gin.H{
+				"error": "TOKEN IS MALFORMED.",
+			})
+			c.Abort()
+			return
+		}
 		token := strings.Split(authHeader, " ")[1]
+
 		tokenClaims, err := jwt.ParseWithClaims(token, &entity.Claims{}, func(token *jwt.Token) (i interface{}, err error) {
 			return jwtSecret, nil
 		})
+
 		if err != nil {
 			var message string
 			if ve, ok := err.(*jwt.ValidationError); ok {
 				if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-					message = "token is malformed"
+					message = "TOKEN IS MALFORMED."
 				} else if ve.Errors&jwt.ValidationErrorUnverifiable != 0 {
-					message = "token could not be verified because of signing problems"
+					message = "TOKEN NOT VERIFIED."
 				} else if ve.Errors&jwt.ValidationErrorSignatureInvalid != 0 {
-					message = "signature validation failed"
+					message = "SIGNATURE VALIDATION FAILED."
 				} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
-					message = "token is expired"
+					message = "TOKEN IS EXPIRED."
 				} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
-					message = "token is not yet valid before sometime"
+					message = "TOKEN IS NOT YET VALID."
 				} else {
 					message = "can not handle this token"
 				}
@@ -53,7 +64,6 @@ func JwtAuth() gin.HandlerFunc {
 		if _, ok := tokenClaims.Claims.(*entity.Claims); ok && tokenClaims.Valid {
 			c.Next()
 		} else {
-			fmt.Println("========================")
 			c.Abort()
 			return
 		}
